ba75987bd571c8168176761f12d176e307d611c9
WRyan <wesley.ryan03@gmail.com>
2024-10-30 14:31:52 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+const makeSelect = $('#make-select');
+const yearSelect = $('#year-select');
+const nextButton = $('#next-button');
+
+fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json')
+    .then(response => response.json())
+    .then(data => {
+        const sortedMakes = data.Results.sort((a, b) => a.MakeName.localeCompare(b.MakeName));
+
+        sortedMakes.forEach(make => {
+            const option = $('<option></option>').val(make.MakeId).text(make.MakeName);
+            makeSelect.append(option);
+        });
+
+        makeSelect.select2({
+            placeholder: 'Selecione a marca do carro',
+            allowClear: true
+        });
+    });
+
+const currentYear = new Date().getFullYear();
+for (let year = 2015; year <= currentYear; year++) {
+    const option = $('<option></option>').val(year).text(year);
+    yearSelect.append(option);
+}
+
+yearSelect.select2({
+    placeholder: 'Selecione o ano',
+    allowClear: true
+});
+
+makeSelect.on('change', validateSelection);
+yearSelect.on('change', validateSelection);
+
+function validateSelection() {
+    if (makeSelect.val() && yearSelect.val()) {
+        nextButton.prop('disabled', false);
+    } else {
+        nextButton.prop('disabled', true);
+    }
+}
+
+nextButton.on('click', () => {
+    const makeId = makeSelect.val();
+    const year = yearSelect.val();
+    window.location.href = `results.html?makeId=${makeId}&year=${year}`;
+});
